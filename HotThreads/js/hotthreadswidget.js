54bc8abb15db48e6bb8453f83f318af6fadda46a
Diego Zanella <businessdad75@gmail.com>
2013-02-12 02:36:27 +0000

    Renamed file

---
+++ HotThreads/js/hotthreadswidget.js
@@ -0,0 +1,39 @@
+/**
+{licence}
+*/
+$(document).ready(function(){
+	/**
+	 * Updates the content of the Hot Threads Widget.
+	 */
+	function UpdateHotThreadsWidget() {
+		var HotThreadsListURL = gdn.url('/plugin/hotthreads/getwidgetcontent');
+
+		$.ajax({
+			url: HotThreadsListURL,
+			global: false,
+			dataType: 'html',
+			success: function(Data){
+				var CurrentItems = $("#HotThreadsList_Items").html();
+				// Update widget only if received data is different from the old one
+				if(CurrentItems != Data) {
+					// Hide current Widget content with a fade effect
+					$("#HotThreadsList_Items").fadeOut('slow', function() {
+						// Replace widget content and display it again, using the same fade effect
+						$(this).html(Data);
+						$("#HotThreadsList_Items").fadeIn('slow');
+					});
+				}
+
+				// Set the auto refresh to occur again later
+				setTimeout(UpdateHotThreadsWidget, gdn.definition('HotThreadsWidget_AutoUpdateDelay') * 1000);
+			}
+		});
+	}
+
+	// If Auto Update delay is greather than zero, it means it's enabled. In such
+	// case, it starts the auto-refresh loop
+	var AutoUpdateDelay = gdn.definition('HotThreadsWidget_AutoUpdateDelay');
+	if(gdn.definition('HotThreadsWidget_AutoUpdateDelay') > 0) {
+		setTimeout(UpdateHotThreadsWidget, gdn.definition('HotThreadsWidget_AutoUpdateDelay') * 1000);
+	}
+});
